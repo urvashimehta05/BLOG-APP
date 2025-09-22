88747cfc772d672f6838ff54c1387e0524704a3a
urvashi <142580122+urvashimehta05@users.noreply.github.com>
2025-09-22 20:42:16 +0530

    .

--- client/BLOG-APP/vite.config.js
+++ client/BLOG-APP/vite.config.js
@@ -3,7 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist'
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'https://blog-app-950d.onrender.com',
+        changeOrigin: true,
+        secure: false
+      }
+    }
   }
 });
