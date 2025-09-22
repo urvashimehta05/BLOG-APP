f2056b06d61b06e8319ded557f0568ba621a481c
urvashi <142580122+urvashimehta05@users.noreply.github.com>
2025-09-22 20:26:40 +0530

    changes

--- client/BLOG-APP/vite.config.js
+++ client/BLOG-APP/vite.config.js
@@ -3,20 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    },
-    cors: {
-      origin: 'http://localhost:5173',
-      credentials: true, // ✅ CRITICAL: enable cookies/sessions
-    }
+  build: {
+    outDir: 'dist'
   }
 });
-
-
--- server/index.js
+++ server/index.js
@@ -45,7 +45,10 @@ mongoose.connect('mongodb://localhost:27017/', {
 })
 .then(() => console.log('✅ Connected to MongoDB'))
 .catch(err => console.error('❌ MongoDB connection error:', err));
-
+app.use(cors({
+  origin: 'https://blog-app-950d.onrender.com',
+  credentials: true
+}));
 app.get('/api/test-auth', (req, res) => {
   if (req.isAuthenticated()) {
     return res.json({
